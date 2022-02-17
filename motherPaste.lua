378c1f94eedadbf6e43ee5085d16a5dfdc113f5e
Narfis <Sebban.hansson@gmail.com>
2022-02-17 14:51:31 +0100

    o

--- motherPaste.lua
+++ motherPaste.lua
@@ -11,11 +11,6 @@ fs.makeDir("scripts/miner.lua")
 fs.makeDir("scripts/woodchuck.lua")
 
 
-<<<<<<< HEAD
 readAll("https://raw.githubusercontent.com/Narfis/MinecraftServerStuff/master/utils.lua", "scripts/utilities.lua")
 readAll("https://raw.githubusercontent.com/Narfis/MinecraftServerStuff/master/woodchuck.lua", "woodchuck.lua")
 readAll("https://raw.githubusercontent.com/Narfis/MinecraftServerStuff/master/miner.lua", "miner.lua")
-=======
-readAll("https://pastebin.com/raw/z34t40W7", "scripts/utils.lua")
-readAll("https://pastebin.com/raw/DKgngctY", "woodchuck.lua")
->>>>>>> ae187be7fd14fb11165c965ad43c1eefec8af4ae
