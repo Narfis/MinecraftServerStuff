7d374c54102eea00d2f76f2a2a0ce39d47eb9f7e
Narfis <Sebban.hansson@gmail.com>
2022-02-18 00:59:57 +0100

    a

--- exosceleton.lua
+++ exosceleton.lua
@@ -1,19 +1,14 @@
 os.loadAPI("libs/utils.lua")
 
-function formation()
+function formation(n)
     local count = 0
-    local a = isInInv("computercraft:turtle_expanded", 1)
+    local a = isInInv(n, 1)
     while a < 16 and a > 0 do
-        a = utils.isInInv("computercraft:turtle_expanded", a+1)
-        count = count + 1
-        if a == -1 then
-            break
-        end
+        a = utils.isInInv(n, a+1)
+        count = count + turtle.getItemCount(a)
     end
     print(count)
-
-    if count > 4 then
-        print("Suuuh")
-    end
-
 end
+
+
+formation("computercraft:turtle_normal")
