9c9c54c43ab66f12766607dd52a2b948022b135b
Narfis <Sebban.hansson@gmail.com>
2022-02-21 22:05:07 +0100

    A

--- mine.lua
+++ mine.lua
@@ -1,5 +1,6 @@
 os.loadAPI("scripts/miner.lua")
 os.loadAPI("libs/utils.lua")
+os.loadAPI("scripts/woodchuck.lua")
 
 ores = {
     "minecraft:gold_ore",
@@ -21,22 +22,53 @@ ores = {
     "minecraft:deepslate_lapis_ore",
     "minecraft:deepslate_redstone_ore"
 }
-
-for i = 1,64 do
-    turtle.dig()
-    turtle.digUp()
-    turtle.forward()
-    miner.check(ores)
-end
-
-for i = 1, 64 do
-    if turtle.detectDown() == false then
-        slot = utils.isInInv("minecraft:cobblestone", 1)
-        if slot ~= -1 then
-            turtle.select(slot)
-            turtle.placeDown()
-        end
+function DoMine()
+    for i = 1,128 do
+        turtle.dig()
+        turtle.digUp()
+        turtle.forward()
+        miner.check(ores)
     end
+    
+    for i = 1, 128 do
+        if turtle.detectDown() == false then
+            slot = utils.isInInv("minecraft:cobblestone", 1)
+            if slot ~= -1 then
+                turtle.select(slot)
+                turtle.placeDown()
+            end
+        end
     turtle.back()
+    end
+end
+print("Give direction, 1 for right and -1 for left")
+dir = tonumber(read())
+while woodchuck.CalcFuel(128) ~= -1 do
+    DoMine()
+    turtle.back()
+    if dir == 1 then
+        turtle.turnLeft()
+        turtle.dig()
+        turtle.forward()
+        turtle.dig()
+        turtle.digUp()
+        turtle.turnLeft()
+        turtle.dig()
+        turtle.forward()
+        turtle.digUp()
+        dir = 0
+
+    elseif dir == 0 then
+        turtle.turnRight()
+        turtle.dig()
+        turtle.forward()
+        turtle.dig()
+        turtle.digUp()
+        turtle.turnRight()
+        turtle.dig()
+        turtle.forward()
+        turtle.digUp()
+        dir = 1
+    end
 end
 
---
+++ treeFarmer.lua
@@ -0,0 +1,20 @@
+function CreateFarm()
+
+end
+
+function Farm()
+
+end
+
+
+
+
+local choise = tonumber(read())
+
+
+if choise == 1 then
+   Farm()
+end
+if choise == 2 then
+    CreateFarm()
+end
