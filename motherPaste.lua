d725257b7b6c9e1a19c35c5455b3e46566e038da
Narfis <Sebban.hansson@gmail.com>
2022-02-22 19:05:45 +0100

    a

--- motherPaste.lua
+++ motherPaste.lua
@@ -15,7 +15,6 @@ readAll("https://raw.githubusercontent.com/Narfis/MinecraftServerStuff/master/ut
 --scripts
 readAll("https://raw.githubusercontent.com/Narfis/MinecraftServerStuff/master/woodchuck.lua", "scripts/woodchuck.lua")
 readAll("https://raw.githubusercontent.com/Narfis/MinecraftServerStuff/master/miner.lua", "scripts/miner.lua")
-readAll("https://raw.githubusercontent.com/Narfis/MinecraftServerStuff/master/exosceleton.lua", "exosceleton.lua")
 readAll("https://raw.githubusercontent.com/Narfis/MinecraftServerStuff/master/mine.lua", "scripts/mine.lua")
 readAll("https://raw.githubusercontent.com/Narfis/MinecraftServerStuff/master/treeFarmer.lua", "treeFarm")
 
