c898d85bffa377011fba57b2c83c63f162586f21
Kasper <kasper.berg1@hotmail.com>
2022-02-28 22:49:00 +0100

    Update digger.lua

--- digger.lua
+++ digger.lua
@@ -1,3 +1,4 @@
+os.loadAPI("libs/utils.lua")
 os.loadAPI("libs/digUtils.lua")
 
 function square()
@@ -11,4 +12,60 @@ function square()
     digUtils.DigRectangle(width, depth, height)
 end
 
-square()
+local woods = {
+    "minecraft:oak_log"
+}
+
+function FarmWood(woodTypes)
+
+    io.write("Width : ")
+    local width = tonumber(read())
+    io.write("Depth : ")
+    local depth = tonumber(read())
+
+    local left = false
+
+    for i = 1, width do
+        for j = 1, depth do
+            local has_block, data = turtle.inspect()
+            if utils.contains(woodTypes, data.name) then
+                ChopTree(woodTypes)
+            end
+            digUtils.TryForward()
+        end
+
+        if left then
+            left = false
+            turtle.turnLeft()
+            digUtils.TryForward()
+            digUtils.TryForward()
+            turtle.turnLeft()
+        else
+            left = true
+            turtle.turnRight()
+            digUtils.TryForward()
+            digUtils.TryForward()
+            turtle.turnRight()
+        end
+
+    end
+end
+
+function ChopTree(woodTypes)
+    local above = false
+    local blocks = 0
+    while above == false do
+        local has_block, data = turtle.inspectUp()
+        if utils.contains(woodTypes, data.name) then
+            turtle.digUp()
+            turtle.up()
+            above = above + 1
+        else
+            above = false
+        end
+    end
+
+    for i = 1, blocks do
+        turtle.down()
+    end
+end
